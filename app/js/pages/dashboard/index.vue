253640d7c6ca586405756ec099087b143a504198
Lukas Mateffy <luke@mateffy.me>
2021-06-14 21:47:02 +0200

    mission-control/#14 responsive dashboard layouts implemented

--- app/js/pages/dashboard/index.vue
+++ app/js/pages/dashboard/index.vue
@@ -1,19 +1,23 @@
 <template>
 	<main class="dashboard-page p-5 md:p-12 h-full">
 		<grid-layout
-			:layout.sync="layout"
+			v-if="dashboards.ready && currentLayout"
+			:layout.sync="currentLayout"
+			:responsive-layouts="responsiveLayouts"
 			:col-num="12"
 			:row-height="30"
-			:is-draggable="editLayout !== null"
-			:is-resizable="editLayout !== null"
+			:is-draggable="editedLayouts !== null"
+			:is-resizable="editedLayouts !== null"
 			:is-mirrored="false"
 			:vertical-compact="true"
 			:margin="[10, 10]"
 			:use-css-transforms="true"
-
+			:responsive="true"
+			:breakpoints="breakpoints"
+			@breakpoint-changed="onBreakpointChanged"
 		>
 			<grid-item 
-				v-for="item in layout"
+				v-for="item in currentLayout"
 				class="p-2"
 				:x="item.x"
 				:y="item.y"
@@ -23,60 +27,77 @@
 				:key="item.i">
 				<component 
 					v-bind:is="item.component"
-					:class="{'pointer-events-none': editLayout !== null}">
+					:class="{'pointer-events-none': editedLayouts !== null}">
 				</component>
 			</grid-item>
 		</grid-layout>
 
 		<top-bar-actions>
-			<top-bar-button v-if="editLayout === null" @click="startEditing">Edit Dashboard</top-bar-button>
+			<top-bar-button v-if="editedLayouts === null" @click="startEditing">Edit Dashboard</top-bar-button>
 			<template v-else>
         		<top-bar-button @click="finishEdit">Save Changes</top-bar-button>
 				<top-bar-button @click="cancelEdit" secondary>Cancel</top-bar-button>
 				<top-bar-button @click="resetDefaults" secondary>Reset</top-bar-button>
+				<top-bar-button secondary>Breakpoint: {{ breakpoint }}</top-bar-button>
 			</template>
 		</top-bar-actions>
 	</main>
 </template>
 <script type="text/javascript">
 import VueGridLayout from 'vue-grid-layout';
-import { invokeAction } from '@socket';
 
 import topBarActions from '@components/portals/top-bar-actions.vue';
 import topBarButton from '@components/controls/top-bar-button.vue';
 
 import composeServiceComponent from '@helpers/compose-service-component';
 
-export default composeServiceComponent('telemetry', composeServiceComponent('dashboards', {
+export default composeServiceComponent('dashboards', {
 	data: () => ({
-		editLayout: null
+		editedLayouts: null,
+		breakpoint: 'lg'
 	}),
 	methods: {
 		startEditing() {
-			this.editLayout = this.serverLayout;
+			this.editedLayouts = this.syncLayouts;
 		},
 		cancelEdit() {
-			this.editLayout = null;
+			this.editedLayouts = null;
 		},
 		finishEdit() {
 			this.$invokeAction('update', {
-				layout: this.editLayout
+				main: this.editedLayouts
 			});
-			this.editLayout = null;
+			this.editedLayouts = null;
 		},
 	    resetDefaults() {
 			this.$invokeAction('reset');
 			this.cancelEdit();
+	    },
+	    onBreakpointChanged(breakpoint) {
+	    	this.breakpoint = breakpoint;
 	    }
 	},
 	computed: {
-		serverLayout() {
+		syncLayouts() {
 			return this.dashboards.ready
-				? this.dashboards.state.layout
-				: [];
+				? this.dashboards.state.main
+				: null ;
 		},
-		layout() {
-			return this.editLayout || this.serverLayout;
+		responsiveLayouts() {
+			return this.editedLayouts || this.syncLayouts;
+		},
+		currentLayout() {
+			return this.responsiveLayouts
+				? this.responsiveLayouts[this.breakpoint]
+				: null;
+		},
+		breakpoints() {
+			return { lg: 1200, md: 996, sm: 670, xs: 536, xxs: 0 };
+		}
+	},
+	watch: {
+		responsiveLayouts() {
+			// refresh?
 		}
 	},
 	components: {
@@ -85,5 +106,5 @@ export default composeServiceComponent('telemetry', composeServiceComponent('das
 		topBarActions,
 		topBarButton
 	}
-}));
+});
 </script>
