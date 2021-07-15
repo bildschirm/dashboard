3ef7972996a9050dbc186967dbac04809f412747
Lukas Mateffy <github@mateffy.me>
2021-07-16 00:42:24 +0200

    Added Spinner component

--- app/js/components/forms/button.vue
+++ app/js/components/forms/button.vue
@@ -1,18 +1,32 @@
 <template>
-	<button class="forms-button relative" :disabled="loading" @click.prevent="$emit('click')">
-		<loading v-if="loading" class="absolute inset-0 text-purple-400" icon-class="w-5" />
+	<button
+		class="forms-button relative"
+		:disabled="loading"
+		@click.prevent="$emit('click')"
+	>
+		<Spinner
+			v-if="loading"
+			class="absolute inset-0 text-purple-400"
+			icon-class="w-5"
+		/>
 		<div :class="{ 'opacity-0': loading }">
 			<slot />
 		</div>
 	</button>
 </template>
 <script type="text/javascript">
+import Spinner from '../loading/Spinner';
+
 export default {
+	name: 'Button',
 	props: {
 		loading: {
 			type: Boolean,
 			default: false
 		}
+	},
+	components: {
+		Spinner
 	}
 }
 </script>
---
+++ app/js/components/loading/Spinner.vue
@@ -0,0 +1,22 @@
+<template>
+	<section class="flex justify-center items-center">
+		<spinner-icon class="fill-current animate-spin" :class="iconClass" />
+	</section>
+</template>
+
+<script>
+import spinnerIcon from '../icons/spinner-icon';
+
+export default {
+	name: 'Spinner',
+	props: {
+		iconClass: {
+			type: String,
+			default: ''
+		}
+	},
+	components: {
+		spinnerIcon
+	}
+};
+</script>
